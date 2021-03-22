8a3fe12dde1ca451c30893dd22bff292b3229717
staticvoi <shruti.suresh16@siesgst.ac.in>
2021-03-22 18:27:04 +0530

    add function call

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -3,9 +3,10 @@ package main;
 //C:\Users\HP\git-local
 //https://github.com/staticvoi/myprojectrepo.git
 public class Calculator {
-
+	//C:\Users\HP\git\myprojectrepo
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		add(10,20);
 	}
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
