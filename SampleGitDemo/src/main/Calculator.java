036099b79e552ee3428ed658d96c11db6253c8b0
staticvoi <68241110+staticvoi@users.noreply.github.com>
2021-03-22 18:29:06 +0530

    added subtract functn

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -11,5 +11,8 @@ public class Calculator {
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
 	  }
+	public static void subtract(int num1,int num2){
+	      System.out.println(num1-num2);
+	  }
 
 }
