0a495e7d7e068da36e8b3a4042e55b8505cfc109
staticvoi <shruti.suresh16@siesgst.ac.in>
2021-03-23 09:55:34 +0530

    added square in br1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -17,5 +17,8 @@ public class Calculator {
 	public static void prod(int num1,int num2){
 	      System.out.println(num1*num2);
 	  }
+	public static void square(int num1){
+	      System.out.println(num1*num1);
+	  }
 
 }
