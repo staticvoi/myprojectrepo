3855159f9702d13c0780e85be968bea3f2fed865
staticvoi <shruti.suresh16@siesgst.ac.in>
2021-03-23 10:33:01 +0530

    added cube method

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -26,7 +26,9 @@ public class Calculator {
 	}
 	public static void square(int num1){
 	      System.out.println(num1*num1);
-
+	  }
+	public static void cube(int num1){
+	      System.out.println(num1*num1*num1);
 	  }
 
 }
